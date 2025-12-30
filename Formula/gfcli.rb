f8255797bd0f44fcb93f9d14353d874960bea201
Tin Sever <mail@tin-sever.de>
2025-12-31 00:43:40 +0100

    Add test for gfcli version and help command
    Add a test block to verify gfcli version and help output

--- Formula/gfcli.rb
+++ Formula/gfcli.rb
@@ -13,4 +13,10 @@ class Gfcli < Formula
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink "#{libexec}/bin/gfcli"
   end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gfcli --version")
+    output = shell_output("#{bin}/gfcli --help")
+    assert_match "Usage: gfcli", output
+  end
 end
