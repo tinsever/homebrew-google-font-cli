d01b940d0dfb0de7101e0a1957e3f4f51ab5cab5
Tin Sever <web@sevetin.net>
2026-01-02 15:01:41 +0100

    update

--- Formula/gfcli.rb
+++ Formula/gfcli.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Gfcli < Formula
   desc "Download and install Google Web Fonts on your local machine"
   homepage "https://github.com/tinsever/google-font-cli"
-  url "https://github.com/tinsever/google-font-cli/archive/refs/tags/v3.0.0.tar.gz"
+  url "https://github.com/tinsever/google-font-cli/archive/refs/tags/v3.1.0.tar.gz"
   sha256 "b5c40edbe1ba45f5f641925b0a543b74545d159d55adad5ee85a0daa590a6feb"
   license "MIT"
 
