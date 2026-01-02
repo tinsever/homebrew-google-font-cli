773ae08adf48bb2471090287d2774b9f54db1787
Tin Sever <web@sevetin.net>
2026-01-02 15:00:52 +0100

    fix tap

--- Formula/gfcli.rb
+++ Formula/gfcli.rb
@@ -1,7 +1,7 @@
 require "language/node"
 
 class Gfcli < Formula
-  desc "Download and install Google Web Fonts on your local machine. Both CLI and API available."
+  desc "Download and install Google Web Fonts on your local machine"
   homepage "https://github.com/tinsever/google-font-cli"
   url "https://github.com/tinsever/google-font-cli/archive/refs/tags/v3.0.0.tar.gz"
   sha256 "b5c40edbe1ba45f5f641925b0a543b74545d159d55adad5ee85a0daa590a6feb"
@@ -10,7 +10,7 @@ class Gfcli < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *Language::Node.std_npm_args(libexec)
     bin.install_symlink "#{libexec}/bin/gfcli"
   end
 
